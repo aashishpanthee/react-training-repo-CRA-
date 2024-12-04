344d7ce392e60e26c5f425dc5a2e5f5e68855c10
Aashish Panthee <aashishpanthee060@gmail.com>
2024-12-04 14:06:27 +0545

    ProductCard & categories shown in ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import Category from './components/Category';
 function App() {
   return (
     <div>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const ProductCard = (props) => {
+const ProductCard = ({ productItem }) => {
   return (
-    <div className='flex flex-col gap-4 px-3 border-2 border-slate-600 w-72'>
-      <img src={props.image} alt='' className='h-80' />
-      <h2 className='text-2xl'>{props.title}</h2>
-      <p>{props.description}</p>
-      <h3 className='text-2xl'>{props.price}</h3>
+    <div className='flex flex-col gap-4 px-4 py-4 transition-shadow duration-300 bg-white border rounded-lg shadow-md border-slate-200 w-72 hover:shadow-xl'>
+      <img
+        src={productItem.image}
+        alt={productItem.title}
+        className='object-cover transition-transform duration-300 rounded-md h-80 hover:scale-105'
+      />
+      <h2 className='text-2xl font-semibold text-gray-800 line-clamp-1'>
+        {productItem.title}
+      </h2>
+      <p className='text-gray-600 line-clamp-2'>{productItem.description}</p>
+      <h3 className='text-2xl font-bold text-indigo-600'>
+        <span className='text-slate-800'>Price: </span> {productItem.price}
+      </h3>
+      <h3 className='text-2xl font-bold text-indigo-600'>
+        <span className='text-slate-800'>Rate: </span>
+        {productItem.rating.rate}
+      </h3>
     </div>
   );
 };
