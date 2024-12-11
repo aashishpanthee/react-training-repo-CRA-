e9778e6e9e8cb8d6669b0baa7c9734e0d1a8cc71
Aashish Panthee <aashishpanthee060@gmail.com>
2024-12-11 13:03:38 +0545

    add router config & add router config to index.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Header from './components/Header';
-import Body from './components/Body';
-import Body1 from './components/Body1';
 import Footer from './components/Footer';
-import Category from './components/Category';
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <div>
       <Header />
-      <Body />
-      {/* <Body1 /> */}
+      <Outlet />
       <Footer />
     </div>
   );
