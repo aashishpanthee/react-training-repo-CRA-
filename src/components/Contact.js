b492ebc89c9f1bd08d2afdda49139357d75dedd9
Aashish Panthee <aashishpanthee060@gmail.com>
2024-12-22 13:41:24 +0545

    add: formik &  yup for form validation in Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Formik } from 'formik';
+import { SignupSchema } from '../utils/validation';
 
-const Contact = () => {
-  return <div>Contact</div>;
-};
+const Contact = () => (
+  <div>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        console.log(values, 'Form submitted values');
+      }}
+    >
+      {({ values, errors, handleChange, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            onChange={handleChange}
+            value={values.email}
+          />
+          {errors.email && <p className='text-red-700'>{errors.email}</p>}
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={handleChange}
+            value={values.password}
+          />
+          {errors.password && <p className='text-red-700'>{errors.password}</p>}
+          <button type='submit'>Submit</button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
 export default Contact;
---
+++ src/utils/validation.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+export const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      'Invalid email address'
+    )
+    .required('Required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .matches(/[0-9]/, 'Password must contain at least one number')
+    .matches(
+      /[@$!%*?&]/,
+      'Password must contain at least one special character'
+    )
+    .required('Required'),
+});
